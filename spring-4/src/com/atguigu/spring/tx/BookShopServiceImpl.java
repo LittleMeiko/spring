af91109551b715dc5f809b5340cfe57525bcadf0
Meiko <957956791@qq.com>
2021-11-18 20:53:09 +0800

    事务的五个属性：传播、隔离级别、回滚、只读、超时

--- spring-4/src/com/atguigu/spring/tx/BookShopServiceImpl.java
+++ spring-4/src/com/atguigu/spring/tx/BookShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.atguigu.spring.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,8 +13,12 @@ public class BookShopServiceImpl implements BookShopService {
 	private BookShopDao bookShopDao;
 	
 	//添加事务注解
-	//使用propagation属性指定事务的传播行为
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	//1.使用propagation属性指定事务的传播行为
+	//2.使用isolation属性指定事务的隔离级别
+	//3.默认情况下Spring的声明式事务对所有的运行时异常进行回滚，也可以通过对应的属性进行设置 noRollbackFor
+	//4.可以通过readOnly属性指定事务是否为只读(为只读时数据库可对只读事务进行优化(不加锁)，提高效率)
+	//5.可以使用timeout属性指定强制回滚之前事务可占用的时间
+	@Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.READ_COMMITTED, readOnly = false)
 	@Override
 	public void purchase(String username, String isbn) {
 		int price = bookShopDao.findBookPriceByIsbn(isbn);
